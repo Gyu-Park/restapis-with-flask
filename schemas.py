11b94dc30f224d860e7091be7f97abb926d907a1
Gyu-Park <kyesung8282@gmail.com>
2023-05-19 15:09:43 -0400

    updated schemas

--- schemas.py
+++ schemas.py
@@ -1,11 +1,17 @@
 from marshmallow import Schema, fields
 
 
-class ItemSchema(Schema):
+# need these plain schemas because without those,
+# ItemSchema and StoreSchema will call each other's Schema infinitely
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
+
+
+class PlainStoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
 
 
 class ItemUpdateSchema(Schema):
@@ -13,6 +19,10 @@ class ItemUpdateSchema(Schema):
     price = fields.Float()
 
 
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)
+
+
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
