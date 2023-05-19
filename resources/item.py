967d0e2ca05470b9a5ff124523df4b8308f74831
Gyu-Park <kyesung8282@gmail.com>
2023-05-19 14:04:55 -0400

    added response annotations

--- resources/item.py
+++ resources/item.py
@@ -11,6 +11,7 @@ blp = Blueprint("items", __name__, description="Operations on items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id]
@@ -25,6 +26,7 @@ class Item(MethodView):
             abort(404, message="Item not found.")
 
     @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
         try:
             item = items[item_id]
@@ -37,13 +39,15 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return items.values()
 
     # @blp.arguments(ItemSchema) validates the JOSN data from client
     # and it calls this post method
     # and passes the validated data through item_data parameter
     @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
     def post(self, item_data):
         for item in items.values():
             if (
